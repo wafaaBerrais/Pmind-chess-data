06a2b8e4d04e4d84d712643dfa7ecdcdce9d7bc3
Chenyu1016 <yeyuyu3131@gmail.com>
2026-01-26 23:44:05 +0100

    Add data files(evaluation information) using Git LFS

--- project file/code/method.py
+++ project file/code/method.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:724dfe3fdd4f26022757b792a9009197063add54241b22855db69edd702fef7c
-size 3500
+oid sha256:61b59db15066cf739b10735244f47db7dd32aac2c68c49dbb9c47af997ff4192
+size 6956
